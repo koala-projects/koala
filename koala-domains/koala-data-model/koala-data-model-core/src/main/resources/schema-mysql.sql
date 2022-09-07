439c120d5f7c50d6b6b8297d75ac2549fd35e721
Houtaroy <Houtaroy@gmail.com>
2022-09-07 17:00:17 +0800

    refactor(数据模型): 更新数据库脚本

--- koala-domains/koala-data-model/koala-data-model-core/src/main/resources/schema-mysql.sql
+++ koala-domains/koala-data-model/koala-data-model-core/src/main/resources/schema-mysql.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS t_meta_data;
-CREATE TABLE t_meta_data
+DROP TABLE IF EXISTS t_metadata;
+CREATE TABLE t_metadata
 (
   id          VARCHAR(36) NOT NULL COMMENT '主键',
   code        VARCHAR(50) NOT NULL COMMENT '元数据代码',
@@ -11,31 +11,30 @@ CREATE TABLE t_meta_data
 DROP TABLE IF EXISTS t_property;
 CREATE TABLE t_property
 (
-  id           VARCHAR(36) NOT NULL COMMENT '主键',
-  code         VARCHAR(50) NOT NULL COMMENT '属性代码',
-  name         VARCHAR(50) NOT NULL COMMENT '属性名称',
-  description  VARCHAR(500) COMMENT '属性描述',
-  type         INT         NOT NULL COMMENT '属性类型',
-  meta_data_id VARCHAR(36) NOT NULL COMMENT '元数据id',
+  id          VARCHAR(36) NOT NULL COMMENT '主键',
+  code        VARCHAR(50) NOT NULL COMMENT '属性代码',
+  name        VARCHAR(50) NOT NULL COMMENT '属性名称',
+  description VARCHAR(500) COMMENT '属性描述',
+  type        INT         NOT NULL COMMENT '属性类型',
+  metadata_id VARCHAR(36) NOT NULL COMMENT '元数据id',
   PRIMARY KEY (id)
 ) COMMENT = '属性表';
 
 DROP TABLE IF EXISTS t_data;
 CREATE TABLE t_data
 (
-  id           VARCHAR(36) NOT NULL COMMENT '主键',
-  meta_data_id VARCHAR(36) NOT NULL COMMENT '元数据id',
+  id          VARCHAR(36) NOT NULL COMMENT '主键',
+  metadata_id VARCHAR(36) NOT NULL COMMENT '元数据id',
   PRIMARY KEY (id)
 ) COMMENT = '数据表';
 
 DROP TABLE IF EXISTS t_data_element;
 CREATE TABLE t_data_element
 (
-  id           VARCHAR(36) NOT NULL COMMENT '主键',
-  code         VARCHAR(50) NOT NULL COMMENT '数据代码',
-  content      VARCHAR(2000) COMMENT '数据内容',
-  data_id      VARCHAR(36) NOT NULL COMMENT '数据id',
-  property_id  VARCHAR(36) NOT NULL COMMENT '属性id',
-  meta_data_id VARCHAR(36) NOT NULL COMMENT '元数据id',
+  id          VARCHAR(36) NOT NULL COMMENT '主键',
+  code        VARCHAR(50) NOT NULL COMMENT '数据代码',
+  content     VARCHAR(2000) COMMENT '数据内容',
+  data_id     VARCHAR(36) NOT NULL COMMENT '数据id',
+  property_id VARCHAR(36) NOT NULL COMMENT '属性id',
   PRIMARY KEY (id)
 ) COMMENT = '数据元表';
