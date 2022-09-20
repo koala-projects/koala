bd9a3a4e9a4c73d8a5aff9b22ef202a1b5f8fcff
Houtaroy <Houtaroy@gmail.com>
2022-09-20 19:17:40 +0800

    feat(模板): Enjoy渲染器增加字符串渲染方法

--- koala-domains/koala-template/src/main/java/cn/koala/template/EnjoyRenderer.java
+++ koala-domains/koala-template/src/main/java/cn/koala/template/EnjoyRenderer.java
@@ -19,4 +19,15 @@ public class EnjoyRenderer implements Renderer {
   public String render(Template template, Map<?, ?> data) {
     return engine.getTemplate(new EnjoyTemplate(template)).renderToString(data);
   }
+
+  /**
+   * 根据字符串模板渲染
+   *
+   * @param content 字符串模板
+   * @param data    数据
+   * @return 渲染结果
+   */
+  public String render(String content, Map<?, ?> data) {
+    return engine.getTemplateByString(content).renderToString(data);
+  }
 }
