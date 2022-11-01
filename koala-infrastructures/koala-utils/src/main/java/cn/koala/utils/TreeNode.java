840a4b2a2eea7434e2e2f90ec52e2708e48ef6d6
Houtaroy <Houtaroy@gmail.com>
2022-11-01 22:45:31 +0800

    feat(工具模块): 增加属性工具类

---
+++ koala-infrastructures/koala-utils/src/main/java/cn/koala/utils/TreeNode.java
@@ -0,0 +1,31 @@
+package cn.koala.utils;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Houtaroy
+ */
+@Data
+public class TreeNode<T> {
+  protected String id;
+  protected String name;
+  protected T data;
+  protected String parentId;
+  protected List<TreeNode<T>> children;
+
+  /**
+   * 构造函数
+   *
+   * @param id       节点id
+   * @param data     节点数据
+   * @param parentId 上级节点id
+   */
+  public TreeNode(String id, String name, String parentId, T data) {
+    this.id = id;
+    this.name = name;
+    this.data = data;
+    this.parentId = parentId;
+  }
+}
