5bdc3b84d0f51638a354e9e747d1fef2522ccea5
Houtaroy <Houtaroy@gmail.com>
2022-11-15 20:40:51 +0800

    feat(JDBC): 增加JDBC模块

---
+++ koala-domains/koala-jdbc/src/main/java/cn/koala/jdbc/JdbcNames.java
@@ -0,0 +1,17 @@
+package cn.koala.jdbc;
+
+/**
+ * @author Houtaroy
+ */
+public interface JdbcNames {
+  String TABLE_CAT = "TABLE_CAT";
+  String TABLE_NAME = "TABLE_NAME";
+  String COLUMN_NAME = "COLUMN_NAME";
+  String COLUMN_TYPE = "DATA_TYPE";
+  String TYPE_NAME = "TYPE_NAME";
+  String COLUMN_SIZE = "COLUMN_SIZE";
+  String DECIMAL_DIGITS = "DECIMAL_DIGITS";
+  String IS_NULLABLE = "IS_NULLABLE";
+  String IS_AUTOINCREMENT = "IS_AUTOINCREMENT";
+  String REMARKS = "REMARKS";
+}
---
+++ koala-domains/koala-jdbc/src/test/java/cn/koala/jdbc/JdbcHelperTest.java
@@ -0,0 +1,26 @@
+package cn.koala.jdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author Houtaroy
+ */
+public class JdbcHelperTest {
+  @Test
+  public void test() throws SQLException {
+    String url = "jdbc:mysql://localhost:3306";
+    String username = "root";
+    String password = "123456";
+    Assertions.assertTrue(JdbcHelper.getCatalogNames(url, username, password).contains("koala"));
+    List<String> tableNames = JdbcHelper.getTableNames(url, username, password, "koala");
+    Assertions.assertTrue(tableNames.contains("t_user"));
+    List<Column> columns = JdbcHelper.getColumns(url, username, password, "koala", "t_role");
+    Assertions.assertFalse(columns.isEmpty());
+    Assertions.assertEquals(columns.get(0).getName(), "id");
+    Assertions.assertTrue(columns.get(0).isPrimaryKey());
+  }
+}
