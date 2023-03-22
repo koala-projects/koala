67cf39517ec75821a7d4b589dd5566424a7aeef9
houtaroy <houtaroy@gmail.com>
2023-03-22 14:43:23 +0800

    refactor(MyBatis): 移除MyBatis过期的类

--- koala-starters/koala-mybatis-starter/src/main/java/cn/koala/mybatis/autoconfigure/MyBatisAutoConfiguration.java
+++ koala-starters/koala-mybatis-starter/src/main/java/cn/koala/mybatis/autoconfigure/MyBatisAutoConfiguration.java
@@ -1,11 +1,8 @@
 package cn.koala.mybatis.autoconfigure;
 
-import cn.koala.mybatis.AuditorIdSupplier;
-import cn.koala.mybatis.CrudHelper;
 import cn.koala.mybatis.EnhancedEnumTypeHandler;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,18 +23,7 @@ public class MyBatisAutoConfiguration {
    * @return 自定义配置
    */
   @Bean
-  public ConfigurationCustomizer enhancedEnumTypeConfigurationCustomizer(
-    ObjectProvider<AuditorIdSupplier<?>> auditorIdSupplier) {
-    auditorIdSupplierCustomizer(auditorIdSupplier.getIfAvailable());
+  public ConfigurationCustomizer enhancedEnumTypeConfigurationCustomizer() {
     return configuration -> configuration.getTypeHandlerRegistry().register(EnhancedEnumTypeHandler.class);
-  }
-
-  /**
-   * 审计员ID提供者定制器
-   *
-   * @param auditorIdSupplier 审计员ID提供者
-   */
-  private void auditorIdSupplierCustomizer(AuditorIdSupplier<?> auditorIdSupplier) {
-    CrudHelper.setAuditorIdSupplier(auditorIdSupplier);
   }
 }
