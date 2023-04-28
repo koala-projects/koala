7b796fd7981ceaf0c671267d25612c34707f3b1d
houtaroy <houtaroy@gmail.com>
2023-04-28 10:53:27 +0800

    fix(附件模块): 删除无用引入

--- koala-domains/koala-attachment/src/main/java/cn/koala/attachment/AttachmentEntity.java
+++ koala-domains/koala-attachment/src/main/java/cn/koala/attachment/AttachmentEntity.java
@@ -1,6 +1,5 @@
 package cn.koala.attachment;
 
-import cn.koala.attachment.Attachment;
 import cn.koala.validation.group.Add;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
