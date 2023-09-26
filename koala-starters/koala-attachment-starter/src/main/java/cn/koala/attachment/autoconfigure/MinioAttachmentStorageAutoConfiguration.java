95b9c9fbde6766b3ac0d5233eb6b34d121ab22d3
houtaroy <houtaroy@gmail.com>
2023-09-26 10:47:05 +0800

    fix(附件模块): 修复对Minio强依赖的问题

--- koala-starters/koala-attachment-starter/src/main/java/cn/koala/attachment/autoconfigure/MinioAttachmentStorageAutoConfiguration.java
+++ koala-starters/koala-attachment-starter/src/main/java/cn/koala/attachment/autoconfigure/MinioAttachmentStorageAutoConfiguration.java
@@ -2,11 +2,7 @@ package cn.koala.attachment.autoconfigure;
 
 import cn.koala.attachment.storage.AttachmentStorage;
 import cn.koala.attachment.storage.support.MinioAttachmentStorage;
-import cn.koala.minio.autoconfigure.MinioAutoConfiguration;
 import io.minio.MinioClient;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -18,13 +14,10 @@ import org.springframework.context.annotation.Configuration;
  * @author Houtaroy
  */
 @Configuration
-@AutoConfigureAfter(MinioAutoConfiguration.class)
 @ConditionalOnProperty(value = "koala.attachment.type", havingValue = AttachmentProperties.TYPE_MINIO)
-@ConditionalOnClass(name = "io.minio.MinioClient")
 public class MinioAttachmentStorageAutoConfiguration {
 
   @Bean
-  @ConditionalOnBean(MinioClient.class)
   @ConditionalOnMissingBean
   public AttachmentStorage minIOAttachmentStorage(MinioClient client) throws Exception {
     return new MinioAttachmentStorage(client);
