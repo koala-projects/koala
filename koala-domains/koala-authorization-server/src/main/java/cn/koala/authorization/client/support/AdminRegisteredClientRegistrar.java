1ad670d2db966998ad9f73fd1981f1a6a595b9ba
houtaroy <houtaroy@gmail.com>
2023-10-24 18:29:22 +0800

    fix(认证授权服务): 修复默认注册客户端回调地址不支持http的问题

--- koala-domains/koala-authorization-server/src/main/java/cn/koala/authorization/client/support/AdminRegisteredClientRegistrar.java
+++ koala-domains/koala-authorization-server/src/main/java/cn/koala/authorization/client/support/AdminRegisteredClientRegistrar.java
@@ -44,7 +44,7 @@ public class AdminRegisteredClientRegistrar implements RegisteredClientRegistrar
       .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
       .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
       .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-      .redirectUri("https://127.0.0.1:3100/authorize")
+      .redirectUri("http://127.0.0.1:3100/authorize")
       .redirectUri("http://127.0.0.1:4200/swagger-ui/oauth2-redirect.html")
       .scope(OidcScopes.OPENID)
       .scope(OidcScopes.PROFILE)
