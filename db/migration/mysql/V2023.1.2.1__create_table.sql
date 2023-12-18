2c58876246775c9a20e3a57702a3fffba9e1948b
Houtaroy <Houtaroy@gmail.com>
2023-12-18 17:47:48 +0800

    fix(附件模块): 提交遗漏的数据库脚本

--- db/migration/mysql/V2023.1.2.1__create_table.sql
+++ db/migration/mysql/V2023.1.2.1__create_table.sql
@@ -57,24 +57,6 @@ CREATE TABLE oauth2_authorization
   PRIMARY KEY (id)
 );
 
--- Attachment
--- 附件表
-CREATE TABLE t_attachment
-(
-  `id`                 BIGINT       NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `original_filename`  VARCHAR(100) NOT NULL COMMENT '原始文件名',
-  `content_type`       VARCHAR(100) NOT NULL COMMENT '文件类型',
-  `size`               INT          NOT NULL COMMENT '文件大小(KB)',
-  `storage_path`       VARCHAR(500) NOT NULL COMMENT '存储路径',
-  `created_by`         BIGINT       NOT NULL COMMENT '创建人ID',
-  `created_time`       DATETIME     NOT NULL COMMENT '创建时间',
-  `last_modified_by`   BIGINT COMMENT '最后更新人ID',
-  `last_modified_time` DATETIME COMMENT '最后更新时间',
-  `deleted_by`         BIGINT COMMENT '删除人ID',
-  `deleted_time`       DATETIME COMMENT '删除时间',
-  PRIMARY KEY (id)
-) COMMENT = '附件表';
-
 -- Query
 -- 查询表
 CREATE TABLE k_query
@@ -434,4 +416,23 @@ CREATE TABLE k_login_log
   PRIMARY KEY (id)
 ) COMMENT = '登录日志表';
 
-
+-- Attachment
+-- 附件表
+CREATE TABLE k_attachment
+(
+  `id`                 BIGINT       NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `original_filename`  VARCHAR(50)  NOT NULL COMMENT '原始文件名',
+  `content_type`       VARCHAR(100) NOT NULL COMMENT '文件类型',
+  `size`               INT          NOT NULL COMMENT '文件大小(KB)',
+  `storage_path`       VARCHAR(100) NOT NULL COMMENT '存储路径',
+  `enabled`            VARCHAR(20)  NOT NULL DEFAULT 'YES' COMMENT '是否启用',
+  `systemic`           VARCHAR(20)  NOT NULL DEFAULT 'NO' COMMENT '是否系统',
+  `deleted`            VARCHAR(20)  NOT NULL DEFAULT 'NO' COMMENT '是否删除',
+  `created_by`         BIGINT       NOT NULL COMMENT '创建人ID',
+  `created_date`       DATETIME     NOT NULL COMMENT '创建时间',
+  `last_modified_by`   BIGINT COMMENT '最后更新人ID',
+  `last_modified_date` DATETIME COMMENT '最后更新时间',
+  `deleted_by`         BIGINT COMMENT '删除人ID',
+  `deleted_date`       DATETIME COMMENT '删除时间',
+  PRIMARY KEY (id)
+) COMMENT = '附件表';
